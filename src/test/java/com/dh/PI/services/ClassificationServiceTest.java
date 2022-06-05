126fe2dfec0a5f3947d34958da0eea37ae3c9bb2
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-06-05 19:12:32 -0300

    create testing CityController

---
+++ src/test/java/com/dh/PI/services/ClassificationServiceTest.java
@@ -0,0 +1,17 @@
+package com.dh.PI.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClassificationServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void saveScore() {
+    }
+}
